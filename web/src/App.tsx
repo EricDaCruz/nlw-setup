01a78fc98e6c31ccb943a89e399e8b1b4578b498
Eric Rafael <rafaeleric40@gmail.com>
2023-01-19 10:07:45 -0300

    :sparkles: feat: Component Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,13 @@
-import './styles/global.css';
+import { Header } from "./components/Header";
+import "./styles/global.css";
+// import {Habit} from "./components/Habit"
 
-import {Habit} from "./Habit"
-
-function App() {
-  return (
-     <div>
-        <Habit completed={3} />
-        <Habit completed={4} />
-        <Habit completed={5} />
-     </div>
-  );
+export function App() {
+   return (
+      <div className="w-screen h-screen flex justify-center items-center">
+         <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+            <Header />
+         </div>
+      </div>
+   );
 }
-
-export default App
