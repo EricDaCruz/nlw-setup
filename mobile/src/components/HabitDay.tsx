cc6a9d07a0ad0360fe33430629a4dbc8d5cc1861
Eric Rafael <rafaeleric40@gmail.com>
2023-01-19 12:41:55 -0300

    :sparkles: feat: HabitDay

---
+++ mobile/src/components/HabitDay.tsx
@@ -0,0 +1,14 @@
+import { TouchableOpacity, Dimensions } from "react-native";
+
+const WEEK_DAYS = 7;
+
+const SCREN_HORZONTAL_PADDING = (32 * 2) / 5;
+
+export const DAY_MARGIN_BETWEEN = 8;
+export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS ) - (SCREN_HORZONTAL_PADDING + 5)
+
+export function HabitDay() {
+   return (
+      <TouchableOpacity className="bg-zinc-900 border-2  border-zinc-800 rounded-lg" style={{width: DAY_SIZE, height: DAY_SIZE}}/>
+   );
+}
