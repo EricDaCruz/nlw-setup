81c6c8fc1927f65194e00fc8049228b66774e0bf
Eric Rafael <rafaeleric40@gmail.com>
2023-01-20 09:40:36 -0300

    :sparkles: feat: Rota de completar e "descompletar"  o habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -68,4 +68,52 @@ export async function appRoutes(app: FastifyInstance) {
 
       res.status(201).send({ possibleHabits, completedHabits });
    });
+
+   app.patch("/habits/:id/toggle", async (req, res) => {
+      const toggleHabitsParam = z.object({
+         id: z.string().uuid(),
+      });
+
+      const { id } = toggleHabitsParam.parse(req.params);
+
+      const today = dayjs().startOf("day").toDate();
+
+      let day = await prisma.day.findUnique({
+         where: {
+            date: today,
+         },
+      });
+
+      if (!day) {
+         day = await prisma.day.create({
+            data: {
+               date: today,
+            },
+         });
+      }
+
+      const dayHabit = await prisma.dayHabit.findUnique({
+         where: {
+            day_id_habit_id: {
+               day_id: day.id,
+               habit_id: id,
+            },
+         },
+      });
+
+      if (dayHabit) {
+         await prisma.dayHabit.delete({
+            where: {
+               id: dayHabit.id,
+            },
+         });
+      } else {
+         await prisma.dayHabit.create({
+            data: {
+               day_id: day.id,
+               habit_id: id,
+            },
+         });
+      }
+   });
 }
